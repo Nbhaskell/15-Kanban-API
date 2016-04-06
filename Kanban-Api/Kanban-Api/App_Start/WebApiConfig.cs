0937558a12b77955be27812e5ae52a7cc4c57c4d
Nathan Haskell <nbhas@Raven.(none)>
2016-04-06 14:59:57 -0700

    updating file paths/nuget package for web broken, refactoring

--- Kanban-Api/Kanban-Api/App_Start/WebApiConfig.cs
+++ Kanban-Api/Kanban-Api/App_Start/WebApiConfig.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace Kanban_Api
 {
@@ -11,7 +12,9 @@ namespace Kanban_Api
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+            var rules = new EnableCorsAttribute("*", "*", "*");
+
+            config.EnableCors(rules);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
